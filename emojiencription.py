a0cd33c5f9145baa05920c4574672022e88b22af
Krsma <vlada.krsmanovic@gmail.com>
2017-12-07 16:40:12 +0100

    formating of code, minor bug fixes, work in progress

--- emojiencription.py
+++ emojiencription.py
@@ -1,14 +1,17 @@
-
 import emoji
 import random
 
-key1 = ['( . Y . )', '눈_눈','(◍•﹏•)','ಠ▃ಠ','ಠ_ಠ','( •_•)','¯\_(ツ)_/¯','ʘ‿ʘ','(⊙_☉)','( ˇ෴ˇ )','( ͡° ͜ʖ ͡°)','(ง •̀_•́)ง','(⌐■_■)']
-key2 = ['•ᴗ•', 'ಥ_ಥ','(　＾∇＾)',' [¬º-°]¬', '|_・)','(~‾▿‾)~','(ᗒᗣᗕ)՞', '◔_◔', '(ᴗ˳ᴗ)','(◣_◢)', '(/•-•)/','( ͡° ͜ʖ ͡°)', 't(-.-t)' ]
+key1 = ['( . Y . )', '눈_눈', '(◍•﹏•)', 'ಠ▃ಠ', 'ಠ_ಠ', '( •_•)', '¯\_(ツ)_/¯', 'ʘ‿ʘ', '(⊙_☉)', '( ˇ෴ˇ )', '( ͡° ͜ʖ ͡°)',
+        '(ง •̀_•́)ง', '(⌐■_■)']
+key2 = ['•ᴗ•', 'ಥ_ಥ', '(　＾∇＾)', ' [¬º-°]¬', '|_・)', '(~‾▿‾)~', '(ᗒᗣᗕ)՞', '◔_◔', '(ᴗ˳ᴗ)', '(◣_◢)', '(/•-•)/',
+        '( ͡° ͜ʖ ͡°)', 't(-.-t)']
 key = []
 existingletters = []
+savekey = []
 content = []
-outputline = ''
+outputline = ""
 outputlist = []
+
 
 def encriptletter(a):
     if a in existingletters:
@@ -19,27 +22,36 @@ def encriptletter(a):
             i = 0
         else:
             i = len(existingletters)
-        return key[i]  # god knows why here exist out of bound error
+        return key[i]
 
-key= key1 + key2
+
+def createoutputkey():
+    outputkey = 0
+    for i in range(len(savekey),0):
+        outputkey = outputkey*10 + key.index(savekey[i])
+    return outputkey
+
+def inputext():
+    for i in range(0, len(content)):
+        text = content[i]
+        text_list = list(text)
+        outputline = ""
+        for j in range(0, len(text_list)):
+            output = encriptletter(text_list[j])
+            outputline = outputline + output
+        outputlist.append(outputline)
+    return outputlist
+
+key = key1 + key2
+savekey = key
 random.shuffle(key)
-existingletters=[]
+existingletters = []
 with open('forencription.txt') as f:
-     content = f.read().splitlines()
+    content = f.read().splitlines()
 
 
-for i in range(0,len(content)):
-    text = content[i]
-    text_list = list(text)
-    for j in range(0, len(text_list)):
-        output = encriptletter(text_list[j])
-        outputline = outputline+output
-    outputlist.append(outputline)
-
-
-#f = open('outputfile.txt', 'w')
-
-for i in range(0,len(outputlist)):
+outputlist = inputext()
+# f = open('outputfile.txt', 'w')
+print(createoutputkey())
+for i in range(0, len(outputlist)):
     print(outputlist[i])
-
-
